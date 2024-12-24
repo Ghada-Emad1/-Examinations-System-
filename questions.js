0230e466f405770356c21a727095fb00aca46463
Ghada Emad <ghadaemad011@gmail.com>
2024-12-24 02:37:24 +0200

    adding images and data of examination system

---
+++ questions.js
@@ -0,0 +1,100 @@
+export const questions=[
+    {
+        title:"Cute Cat",
+        image:"/images/cat.jpg",
+        question:"what is the behaviour of Cat ??",
+        answer1:"Cats is So dangerous",
+        answer2:"Cats behave so weird to their owner",
+        answer3:"Cats is So cute",
+        correctAnswer:"answer3"
+    
+    },
+    {
+        title:"Dog",
+
+        question:"What is a common feature of all dogs?",
+        image:"./images/dog.webp",
+        answer1:"They can purr like cats",
+        answer2:"They have four legs.",
+        answer3:"They all have blue fur.",
+        correctAnswer:"answer2"
+
+    },
+    {
+        title:"Sky",
+        image:"./images/sky.webp",
+        question:"What color is the sky typically on a clear day?",
+        answer1:"Green",
+        answer2:"Purple",
+        answer3:"Blue",
+        correctAnswer:"answer3"
+    },
+    {
+        title:"Green Color",
+        image:"./images/green.jpeg",
+        question: "Which of the following is commonly green?",
+        answer1: "A carrot",
+        answer2:"Grass", 
+        answer3:"The sky",
+        correctAnswer:"answer2"
+        
+    },
+    {
+        title:"Fruits",
+        image:"./images/fruits.jpeg",
+        question:"What is the name of the fruit that shares its name with a color?",
+        answer1:"Banana",
+        answer2:"Orange",
+        answer3:"Apple",
+        correctAnswer:"answer2"
+    },
+    {
+        title:"Elephant",
+        image:"./images/elephant.jpeg",
+        question:"What is a unique feature of elephants?",
+        answer1:"They have trunks",
+        answer2:"They can fly",
+        answer3:"They have stripes",
+        correctAnswer:"answer1"
+    },
+    {
+        title:"Zebra",
+        image:"./images/zebra.jpeg",
+        question:"What is a defining feature of a zebra?",
+        answer1:"Solid White Color",
+        answer2:"Big ears like elephants",
+        answer3:"Stripes",
+        correctAnswer:"answer3"
+    },
+    {
+        title:"pump jacket",
+        image:"./images/pump.jpg",
+        question:"What is a puffer jacket primarily designed for?",
+        answer1:"Providing warmth in cold weather",
+        answer2:"Being waterproof for swimming",
+        answer3:"used as formal wear",
+        correctAnswer:"answer1"
+   
+    },
+    {
+        title:"Orange",
+        image:"./images/orange.jpeg",
+        question:"What is the primary color of an orange fruit?",
+        answer1:"Red",
+        answer2:"Yellow",
+        answer3:"Orange",
+        correctAnswer:"answer3"
+    },
+    {
+        title:"Hoodie",
+        image:"./images/hoodie.jpg",
+        question:"What makes a hoodie different from a regular shirt?",
+        answer1:"It has a zipper",
+        answer2:"It has no sleeves",
+        answer3:"It has a hood",
+        correctAnswer:"answer3"
+    },
+
+
+
+]
